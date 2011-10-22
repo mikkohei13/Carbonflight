014318f42c7ed0e9be285d846ac2cc34d1e00ae7
Mikko Heikkinen <hawk@biomi.org>
2011-10-22 14:13:21 +0300

    TV & Beef added, styling

--- show/index.php
+++ show/index.php
@@ -30,7 +30,18 @@ $array = json_decode($json, TRUE);
 //print_r ($array);
 
 $years = round($array['conversion']['amount'] / 8 / 365, 1);
-$comparison['laptop'] = "<p>" . $years . " years using a laptop 8 h per day</p>";
+$comparison['laptop'] = "<p>" . $years . " years using a laptop, 8 h per day</p>";
+
+
+// TV
+$url = "http://carbon.to/tv.json?co2=$co2";
+$json = file_get_contents($url);
+$array = json_decode($json, TRUE);
+
+//print_r ($array);
+
+$years = round($array['conversion']['amount'] / 8 / 365, 1);
+$comparison['tv'] = "<p>" . $years . " years of watching a TV, 8 hours a day (32-inch flatscreen)</p>";
 
 
 // Car
@@ -41,6 +52,17 @@ $array = json_decode($json, TRUE);
 //print_r ($array);
 
 $comparison['car'] = "<p>" . $array['conversion']['amount'] . " km driving by a car</p>";
+
+
+// Beef
+$url = "http://carbon.to/beef.json?co2=$co2";
+$json = file_get_contents($url);
+$array = json_decode($json, TRUE);
+
+//print_r ($array);
+
+$comparison['beef'] = "<p>" . $array['conversion']['amount'] . " kg of cooked beef</p>";
+
 
 // Beers
 $url = "http://carbon.to/beers.json?co2=$co2";
@@ -82,15 +104,7 @@ while ($i < $array['conversion']['amount'])
 <?php
 
 // echo "<h1><strong>$co2 kg CO<sub>2</sub></strong> is <strong>$percentage %</strong> of your yearly emissions<h1>";
-echo "<h1><strong>$co2 kg CO<sub>2</sub></strong> adds <strong>$percentageAdd %</strong> to your yearly emissions<h1>\n\n";
-
-echo "<h3>$co2 kg CO<sub>2</sub> equals to:</h3>\n\n";
-
-foreach ($comparison as $key => $p)
-{
-	echo $p . "\n";
-}
-
+echo "<h1><strong>$co2 kg CO<sub>2</sub></strong> adds <strong>$percentageAdd %</strong> to your yearly emissions*<h1>\n\n";
 
 ?>
 
@@ -98,14 +112,28 @@ foreach ($comparison as $key => $p)
 
 	<section><div>
 	
+<?php
 
+echo "<h3>$co2 kg CO<sub>2</sub> equals to</h3>\n\n";
+
+foreach ($comparison as $key => $p)
+{
+	echo $p . "\n";
+}
+
+?>
 	
 	</div></section>
 	
 	<footer><div>
 	
+	<p>*Sweden's carbon footprint is 8400 kg per capita</p>
+	
+	<h4>Sources</h4>
+	
 	<p>Flight emissions: <a href="https://www.carbonplanet.com/shop/offsets?offset_type=Flight">Carbon Planet</a></p>
-	<p>Swedish carbon footprint: <a href="http://www.wwf.se/source.php/1211289/An_Analysis_of_Swedens_Carbon_Footprint.pdf">WWF: An Analysis of Sweden's Carbon Footprint</a></p>
+	<p>Sweden's carbon footprint: <a href="http://www.wwf.se/source.php/1211289/An_Analysis_of_Swedens_Carbon_Footprint.pdf">WWF: An Analysis of Sweden's Carbon Footprint</a></p>
+	<p>Carbon comparison: <a href="http://carbon.to/">Carbon.to</a></p>
 
 	</div></footer>
 	
